436b28ae5c9910742405cb8c0944b230629181b7
Your Name~ <you@example.com>
2024-01-04 13:22:55 +0600

    Fast API Tutorial, Part 4: Request Body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from enum import Enum
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -56,3 +57,19 @@ async def get_user_item(
             }
         )
         return item
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+@app.post("/items")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
