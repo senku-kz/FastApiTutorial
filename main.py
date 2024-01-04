240fa5418a16a062655e0ecc1646d3b83dd53491
Your Name~ <you@example.com>
2024-01-04 13:01:42 +0600

    Fast API Tutorial, Part 3: Query Parameters

--- main.py
+++ main.py
@@ -5,26 +5,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
-
-
-@app.post("/")
-async def post():
-    return {"message": "hello from the post route"}
-
-
-@app.put("/")
-async def put():
-    return {"message": "hello from the put route"}
-
-
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
@@ -35,11 +15,11 @@ async def list_items(skip: int = 0, limit: int = 10):
 
 @app.get("/items/{item_id}")
 async def get_item(
-    item_id: str,
-    # q: Optional[str] = None
-    q: str | None = None,
-    short: bool = False
-    # sample_query_param: str, q: str | None = None, short: bool = False
+        item_id: str,
+        # q: Optional[str] = None
+        q: str | None = None,
+        short: bool = False
+        # sample_query_param: str, q: str | None = None, short: bool = False
 ):
     # if q:
     #     return {"item_id": item_id, "q": q}
@@ -57,36 +37,20 @@ async def get_item(
     return item
 
 
-@app.get("/users")
-async def get_user_list():
-    return {"message": "user list"}
-
-
-@app.get("/users/me")
-async def get_current_user():
-    return {"user_id": "this is the current user"}
-
-
-@app.get("/users/{user_id}")
-async def get_user(user_id: str):
-    return {"user_id": user_id}
-
-
-class FoodEnum(str, Enum):
-    fruits = "fruits"
-    vegetables = "vegetables"
-    dairy = "dairy"
-
-
-@app.get("/foods/{food_name}")
-async def get_food(food_name: FoodEnum):
-    if food_name == FoodEnum.vegetables:
-        return {"food_name": food_name, "message": "you are healthy"}
-
-    if food_name.value == "fruits":
-        return {
-            "food_name": food_name,
-            "message": "you are still healthy, but like sweet things",
-        }
-
-    return {"food_name": food_name, "message": "i like chocolate milk"}
+@app.get("/items/{user_id}/item/{item_id}")
+async def get_user_item(
+        user_id: int,
+        item_id: str,
+        q: str | None = None,
+        short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+        return item
