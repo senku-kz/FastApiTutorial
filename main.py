fc0e637a0250d70dced1e463270f85d4bbb7ba2b
Your Name~ <you@example.com>
2024-01-04 09:10:40 +0600

    Fast API Tutorial, Part 1: Introduction

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+@app.post("/")
+async def post():
+    return {"message": "hello from the post route"}
+
+
+@app.put("/")
+async def put():
+    return {"message": "hello from the put route"}
