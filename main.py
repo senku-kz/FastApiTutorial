87a4b98b2609dbb0bf9cb1750435f505df551cb6
Your Name~ <you@example.com>
2024-01-04 12:53:39 +0600

    Fast API Tutorial, Part 3: Query Parameters

--- main.py
+++ main.py
@@ -37,23 +37,25 @@ async def list_items(skip: int = 0, limit: int = 10):
 async def get_item(
     item_id: str,
     # q: Optional[str] = None
-    q: str | None = None
-
+    q: str | None = None,
+    short: bool = False
     # sample_query_param: str, q: str | None = None, short: bool = False
 ):
-    # item = {"item_id": item_id, "sample_query_param": sample_query_param}
     # if q:
-    #     item.update({"q": q})
-    # if not short:
-    #     item.update(
-    #         {
-    #             "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
-    #         }
-    #     )
-    # return item
+    #     return {"item_id": item_id, "q": q}
+    # return {"item_id": item_id}
+
+    item = {"item_id": item_id}
     if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+    return item
+
 
 @app.get("/users")
 async def get_user_list():
