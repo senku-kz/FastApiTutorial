6afb81922be0c930ba1b51f7e715c528b2ff46d3
Your Name~ <you@example.com>
2024-01-04 13:28:40 +0600

    Fast API Tutorial, Part 4: Request Body

--- main.py
+++ main.py
@@ -73,3 +73,15 @@ async def create_item(item: Item):
         price_with_tax = item.price + item.tax
         item_dict.update({"price_with_tax": price_with_tax})
     return item_dict
+
+
+@app.put("/items/{item_id}")
+async def create_item_with_put(
+        item_id: int,
+        item: Item,
+        q: str | None = None
+):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
